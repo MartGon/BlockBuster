ee666376935f24ed8c5ab0c0984132d9a65d4aca
martingonzalezh <martingonzalezh@alumnos.uvigo.es>
2022-03-02 12:23:11 +0100

    Bugfixes Deg Interpolation

--- src/common/math/Interpolation.cpp
+++ src/common/math/Interpolation.cpp
@@ -4,7 +4,7 @@ float Math::InterpolateDeg(float left, float right, float alpha)
 {   
     left = glm::radians(left);
     right = glm::radians(right);
-    auto cs = (1 - alpha) * glm::cos(left) + alpha * glm::cos(right);
-    auto sn = (1 - alpha) * glm::sin(left) + alpha * glm::sin(right);
+    auto cs = alpha * glm::cos(left) + (1 - alpha) * glm::cos(right);
+    auto sn = alpha * glm::sin(left) + (1 - alpha) * glm::sin(right);
     return glm::degrees(std::atan2(sn, cs));
 }
